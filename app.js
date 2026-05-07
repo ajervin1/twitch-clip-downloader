de39036592958b2064e1789a72fad149cfb29bf8
eclaervin <93540020+eclaervin@users.noreply.github.com>
2026-05-07 13:09:51 -0700

    Sorting clips by date, newest clips at the top

--- app.js
+++ app.js
@@ -5,8 +5,21 @@ function saveClip(clip) {
     const savedClips = JSON.parse(fs.readFileSync("saved-clips.json", "utf-8"))
     const isSaved = savedClips.some(savedClip => savedClip.id === clip.id)
     if (!isSaved) {
+        const date = new Date(clip.createdAt)
+        const dateString = date.toDateString() + " " + date.toLocaleTimeString();
         console.log("Saving clip")
-        savedClips.push(clip)
+        savedClips.push({
+            id: clip.id,
+            title: clip.title,
+            displayDate: dateString,        // Readable format
+            timestamp: clip.createdAt,      // Original ISO format for sorting
+            durationSeconds: clip.durationSeconds,
+            mp4Url: clip.mp4Url,
+        })
+
+        // Sort: Newest first based on the original timestamp
+        savedClips.sort((a, b) => new Date(b.timestamp) - new Date(a.timestamp));
+
     } else {
         console.log("Already saved")
     }
@@ -15,7 +28,7 @@ function saveClip(clip) {
 
 
 async function main() {
-    const {clips} = await getUserClips("stpeach", "LAST_WEEK", 50);
+    const {clips} = await getUserClips("stpeach", "LAST_WEEK", 10);
     console.log(clips.length)
     for (const clip of clips) {
         saveClip(clip);
--- utility/fileManager.js
+++ utility/fileManager.js
@@ -1,31 +1,7 @@
-import {fetchClips} from "../twitch/getUserClips.js";
-
 async function downloadClip(clip) {
     console.log(`⏳ Starting download: ${clip.title}`); // ✅ Correct
     const response = await fetch(clip.mp4Url);
     const arrayBuffer = await response.arrayBuffer();
     const buffer = Buffer.from(arrayBuffer);
     return buffer;
-}
-
-function getClipIds() {
-    const json = fs.readFileSync(DOWNLOAD_FILE, 'utf-8');
-    const clipIdsArray = JSON.parse(json);
-    return clipIdsArray;
-}
-
-function addClipId(clipId) {
-    const existingIds = getClipIds();
-    const idsSet = new Set(existingIds);
-    idsSet.add(clipId);
-    const updatedIds = Array.from(idsSet);
-    const jsonIds = JSON.stringify(updatedIds, null, 2);
-    fs.writeFileSync(DOWNLOAD_FILE, jsonIds);
-    return updatedIds;
-}
-
-/* Clip Service */
-function filterNewClips(clipIds, clips) {
-    const newClips = clips.filter(clip => !clipIds.includes(clip.id))
-    return newClips;
 }
